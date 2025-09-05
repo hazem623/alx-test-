1818fcdad5fdc5746dabf68a496e6b69397ee981
Hazem <alnmrh19@gmail.com>
2025-09-04 18:14:37 -0700

    Update file the main.css & main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("Hello, World!");
+
+function greet(name, age) {
+    return `Hello, my name is ${name} and I am ${age} years old.`;
+}
+
+console.log(greet("Hazem", 20));
+
+function add(start, end) {
+    for (let i = start; i <= end; i++) {
+        console.log(i);
+    if (i === end) {
+        return "Done";
+    }
+}
+}
+
+console.log(add(2005, 2025));
